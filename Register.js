14be2538e58beaacdcb0b59b36abad38535c4759
Mateusz Wardawa <2wardawa@pascal23.fis.agh.edu.pl>
2025-01-05 18:00:37 +0100

    add login to website

--- Register.js
+++ Register.js
@@ -1,4 +1,4 @@
-document.getElementById("RegisterForm").addEventListener("submit",async function (event){
+document.getElementById("registerButton").addEventListener("click",async function (event){
     event.preventDefault();
 
     const username=document.getElementById("username").value;
@@ -16,6 +16,34 @@ document.getElementById("RegisterForm").addEventListener("submit",async function
         const messageElement = document.getElementById("message");
         if(response.ok)
             messageElement.innerHTML =result.message;
+        else
+            messageElement.innerHTML = `Błąd: ${result.error}`;
+    }
+    catch(error){
+        console.error("Błąd; " ,error);
+    }
+})
+document.getElementById("loginButton").addEventListener("click",async function (event) {
+    event.preventDefault();
+
+    const username=document.getElementById("username").value;
+    const password=document.getElementById("password").value;
+
+    try{
+        const response = await fetch('http://localhost:9013/login',{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        })
+        const result =await response.json();
+        const messageElement = document.getElementById("message");
+        if(response.ok){
+            messageElement.innerHTML =result.message;
+            localStorage.setItem("token", result.token);
+            window.location.href = "index.html";
+        }
         else
             messageElement.innerHTML = `Błąd: ${result.error}`;
     }
